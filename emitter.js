944d278317f04c3910baa809f762fb7bab5a0601
ADruzhinin <mcfriend@yandex.ru>
2017-10-28 19:00:04 +0500

    first try

--- emitter.js
+++ emitter.js
@@ -7,11 +7,29 @@
 getEmitter.isStar = true;
 module.exports = getEmitter;
 
+function isEmitting(subscriber) {
+    if ('options' in subscriber && 'frequency' in subscriber.options) {
+        let currentFrequency = subscriber.options.currentFrequency;
+        subscriber.options.currentFrequency++;
+
+        return currentFrequency % subscriber.options.frequency === 0;
+    }
+    if ('options' in subscriber && 'times' in subscriber.options) {
+        subscriber.options.times--;
+
+        return subscriber.options.times >= 0;
+    }
+
+    return true;
+}
+
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    let events = {};
+
     return {
 
         /**
@@ -19,26 +37,66 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @param {Object} options
+         * @returns {Object}
          */
-        on: function (event, context, handler) {
-            console.info(event, context, handler);
+        on: function (event, context, handler, options) {
+            if (!(event in events)) {
+                events[event] = [];
+            }
+            events[event].push({
+                context,
+                handler: handler.bind(context)
+            });
+            if (options) {
+                let numberOfLastSubscriber = events[event].length - 1;
+                events[event][numberOfLastSubscriber].options = options;
+            }
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            let eventsForDelete = Object.keys(events).filter(
+                storedEvent => storedEvent.indexOf(event + '.') === 0 || event === storedEvent);
+            eventsForDelete.forEach(eventForDelete => {
+                events[eventForDelete] = events[eventForDelete].filter(
+                    subscriber => subscriber.context !== context);
+            });
+
+            return this;
         },
+
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            let filteredNames = [];
+            let countDots = (event.lastIndexOf('.') !== -1) ? event.match(/\./g).length : 0;
+            for (let i = 0; i < countDots + 1; i++) {
+                if (event in events) {
+                    filteredNames.push(event);
+                }
+                event = event.slice(0, event.lastIndexOf('.'));
+            }
+            filteredNames.forEach(eventForCall => {
+                events[eventForCall].forEach(subscriber => {
+                    if (isEmitting(subscriber)) {
+                        subscriber.handler();
+                    }
+                });
+            });
+
+            return this;
         },
 
         /**
@@ -48,9 +106,12 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object}
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            let options = times > 0 ? { times } : { times: Infinity };
+
+            return this.on(event, context, handler, options);
         },
 
         /**
@@ -60,9 +121,14 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object}
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            let options = frequency > 0
+                ? { frequency, currentFrequency: 0 }
+                : { frequency: 1, currentFrequency: 0 };
+
+            return this.on(event, context, handler, options);
         }
     };
 }
